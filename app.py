73efa77811ec4a633a4b5c678e86f50637655868
ian-manaog <imanaog2272@gmail.com>
2021-05-16 19:26:12 +0300

    string to array

--- app.py
+++ app.py
@@ -9,12 +9,14 @@ vectorizer,classifier = load_objects() #load the model and vectorizer
 def index():
     return render_template('form.html')
 
-@app.route('/predict',methods=['POST']) #prediction on data
+@app.route('/',methods=['POST']) #prediction on data
 def predict():
+    t_sentiment = ''
     text = request.form['text'] #input is from forms
     preptext = preprocess(text) #cleaning and preprocessing of the texts
 
-    if preptext.shape[1] != 0:
+    if preptext: #preptext is true when not empty
+        preptext = np.array([preptext])
         vector_text = vectorizer.transform(preptext) #vectorize text
         predictions = classifier.predict(vector_text) #making predictions
         sentiment = int(np.argmax(predictions)) #index of maximum prediction
@@ -23,11 +25,10 @@ def predict():
             t_sentiment = 'negative'
         elif sentiment==1:
             t_sentiment = 'postive'
-        elif sentiment==2:
-            t_sentiment='neutral'
+        #no neutral class was found in the dataset used for training
         
         return render_template('form.html', sentiment=t_sentiment, text=text)
     #cases where after text preprocessing, output is empty string
     else:
-        return render_template('form.html', sentiment='neutral', text=text)
+        return render_template('form.html', sentiment='none', text=text)
     
