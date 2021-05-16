78b1d9d58325ccad43a7c641d2d28141cba10c8f
ian-manaog <imanaog2272@gmail.com>
2021-05-16 20:36:42 +0300

    nltk download

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+import nltk
 from flask import Flask, request, render_template
 from helpers.utils import *
 app = Flask(__name__)
 
+nltk.download('english')
 vectorizer,classifier = load_objects() #load the model and vectorizer
 
 
