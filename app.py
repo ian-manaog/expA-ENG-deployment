14ae6fabbb2450d2b4e78b6e295e16894368af02
ian-manaog <imanaog2272@gmail.com>
2021-05-16 14:52:37 +0300

    added startup commands

--- app.py
+++ app.py
@@ -9,22 +9,23 @@ vectorizer,classifier = load_objects() #load the model and vectorizer
 def index():
     return render_template('form.html')
 
-@app.route('/predict',methods=['POST']) #prediction on data
+@app.route('/',methods=['POST']) #prediction on data
 def predict():
+    t_sentiment=''
     text = request.form['text'] #input is from forms
     preptext = preprocess(text) #cleaning and preprocessing of the texts
 
-    if preptext.shape[1] != 0:
+    #preptext is true when it is not empty
+    if preptext:
+        preptext = np.array([preptext]) #change to list
         vector_text = vectorizer.transform(preptext) #vectorize text
-        predictions = classifier.predict(vector_text) #making predictions
+        predictions = classifier.predict_proba(vector_text) #making predictions
         sentiment = int(np.argmax(predictions)) #index of maximum prediction
-        probability = max(predictions.tolist()[0]) #probability of maximum prediction
         if sentiment==0: #assigning appropriate name to prediction
             t_sentiment = 'negative'
         elif sentiment==1:
             t_sentiment = 'postive'
-        elif sentiment==2:
-            t_sentiment='neutral'
+        #In training the model, there was no neutral class in the dataset so only two classes are available
         
         return render_template('form.html', sentiment=t_sentiment, text=text)
     #cases where after text preprocessing, output is empty string
