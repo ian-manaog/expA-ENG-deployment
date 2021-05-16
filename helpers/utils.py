d0c1908bb602d47b0b7c7ced32e895723c981e1d
ian-manaog <imanaog2272@gmail.com>
2021-05-16 21:00:34 +0300

    startup command and nltk

--- helpers/utils.py
+++ helpers/utils.py
@@ -4,14 +4,9 @@ import json
 import string
 import pickle
 import numpy as np
-import nltk
-
-nltk.download('english')
-
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
 
 contraction_json = open('helpers/contraction.json',)
