bd04c3a6060bfbd77c12d719ff9242ff1c1ce2d4
ian-manaog <imanaog2272@gmail.com>
2021-05-16 04:15:39 +0300

    web functionality

--- app.py
+++ app.py
@@ -1,6 +1,36 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request, render_template
+from helpers.utils import *
+app = Flask('SA-ENG')
+
+vectorizer,classifier = load_objects() #load the model and vectorizer
+
 
 @app.route("/")
 def index():
     return "<h1>Hello Azure!</h1>"
+"""
+def index():
+    return render_template('form.html')"""
+
+@app.route('/predict',methods=['POST']) #prediction on data
+def predict():
+    text = request.form['text'] #input is from forms
+    preptext = preprocess(text) #cleaning and preprocessing of the texts
+
+    if preptext.shape[1] != 0:
+        vector_text = vectorizer.transform(preptext) #vectorize text
+        predictions = classifier.predict(vector_text) #making predictions
+        sentiment = int(np.argmax(predictions)) #index of maximum prediction
+        probability = max(predictions.tolist()[0]) #probability of maximum prediction
+        if sentiment==0: #assigning appropriate name to prediction
+            t_sentiment = 'negative'
+        elif sentiment==1:
+            t_sentiment = 'postive'
+        elif sentiment==2:
+            t_sentiment='neutral'
+        
+        return render_template('form.html', sentiment=t_sentiment, text=text)
+    #cases where after text preprocessing, output is empty string
+    else:
+        return render_template('form.html', sentiment='neutral', text=text)
+    
---
+++ helpers/utils.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import re
+import json
+import string
+import pickle
+import numpy as np
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+contraction_json = open('helpers/contraction.json',)
+contraction_map = json.load(contraction_json)
+stop_words = stopwords.words("english")
+stemmer = SnowballStemmer("english")
+
+def rep_func(prep_texts): 
+    vectorizer = CountVectorizer()
+    X = vectorizer.fit_transform(prep_texts)
+    return X, vectorizer
+
+def bal_data(df, classes): #used when dataset is not balanced
+    g = df.groupby(classes)
+    g = pd.DataFrame(g.apply(lambda x: x.sample(g.size().min(), random_state=1234).reset_index(drop=True)))
+    return g
+
+def contraction(word):
+    for contraction, replacement in contraction_map.items():
+        word = word.replace(contraction, replacement)
+    return word
+
+def preprocess(text):
+    text = text.lower()
+    #removables-1
+    text = re.sub(r"((http://)[^ ]*|(https://)[^ ]*|(www\.)[^ ]*)",'',text) #links
+    text = re.sub('@[^\s]+','', text) #usernames
+    #emojis are already removed in this dataset
+    text = re.sub('#[^\s]+','', text) #hastags
+    #text = re.sub('^a-z0-9<>', '', text) #remove non-alphanumeric symbols
+    text = re.sub('[0-9]+', '', text) #numbers
+    #transformations-1
+    text = " ".join([contraction(word) for word in text.split()])
+    #removables-2
+    text = ''.join([c for c in text if c not in string.punctuation])#punctuations
+    #transformations-2
+    text = " ".join([stemmer.stem(word) for word in text.split()])
+    text = " ".join([word for word in text.split() if word not in stop_words]) #" ".join(text), text is modified using list comprehension
+    
+    return text
+
+def load_objects():
+    file = open("vectorizer.pkl",'rb')
+    vectorizer = pickle.load(file)
+    file.close()
+    file = open("expA.pkl",'rb')
+    classifier = pickle.load(file)
+    file.close()
+    return vectorizer, classifier
